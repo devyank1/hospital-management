5f41351a62e72eb1103d6c275286be8c6e860c24
Yan Carlos <contact@yancarlos.tech>
2025-05-21 17:35:05 -0300

    Mapper actualized.

--- api-medical/src/main/java/com/hospital/api_medical/mappers/PatientMapper.java
+++ api-medical/src/main/java/com/hospital/api_medical/mappers/PatientMapper.java
@@ -4,10 +4,11 @@ import com.hospital.api_medical.dto.PatientDTO;
 import com.hospital.api_medical.entity.Patient;
 import org.mapstruct.Mapper;
 
+
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
 
-    PatientDTO toDTO(Patient patient);
+    PatientDTO toDto(Patient patient);
 
     Patient toEntity(PatientDTO patientDTO);
 }
