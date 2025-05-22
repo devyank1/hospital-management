b890fb158372322381471ce13625a39bc04d723d
Yan Carlos <contact@yancarlos.tech>
2025-05-22 17:34:30 -0300

    Secutiry Updated.

--- api-medical/src/main/java/com/hospital/api_medical/configs/SecurityConfig.java
+++ api-medical/src/main/java/com/hospital/api_medical/configs/SecurityConfig.java
@@ -8,19 +8,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(csrf -> csrf.disable())
+        return http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/patients/signup", "/login").permitAll()
-                        .anyRequest().authenticated());
-
-        return http.build();
+                        .requestMatchers("/patients/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .build();
     }
 
     @Bean
