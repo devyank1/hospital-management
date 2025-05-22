638992d383b21a06f7f73c9dffd1ba5aea976d49
Yan Carlos <contact@yancarlos.tech>
2025-05-22 17:17:09 -0300

    All mappers Impl working

--- api-medical/target/generated-sources/annotations/com/hospital/api_medical/mappers/MedicDisponibilityMapperImpl.java
+++ api-medical/target/generated-sources/annotations/com/hospital/api_medical/mappers/MedicDisponibilityMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-29T13:04:17-0300",
+    date = "2025-05-22T16:35:10-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
--- api-medical/target/generated-sources/annotations/com/hospital/api_medical/mappers/PatientMapperImpl.java
+++ api-medical/target/generated-sources/annotations/com/hospital/api_medical/mappers/PatientMapperImpl.java
@@ -1,21 +1,23 @@
 package com.hospital.api_medical.mappers;
 
+import com.hospital.api_medical.dto.PatientCreateDTO;
 import com.hospital.api_medical.dto.PatientDTO;
 import com.hospital.api_medical.entity.Patient;
 import java.util.Date;
 import javax.annotation.processing.Generated;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-29T11:52:53-0300",
+    date = "2025-05-22T16:35:10-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
 public class PatientMapperImpl implements PatientMapper {
 
     @Override
-    public PatientDTO toDTO(Patient patient) {
+    public PatientDTO toDto(Patient patient) {
         if ( patient == null ) {
             return null;
         }
@@ -34,16 +36,14 @@ public class PatientMapperImpl implements PatientMapper {
     }
 
     @Override
-    public Patient toEntity(PatientDTO patientDTO) {
+    public Patient toEntity(PatientCreateDTO patientDTO, PasswordEncoder encoder) {
         if ( patientDTO == null ) {
             return null;
         }
 
         Patient patient = new Patient();
 
-        patient.setId( patientDTO.id() );
-        patient.setName( patientDTO.name() );
-        patient.setBirthDate( patientDTO.birthDate() );
+        patient.setPassword( encoder.encode(patientDTO.password()) );
 
         return patient;
     }
